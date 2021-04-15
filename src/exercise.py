e7e95c18b612467c198f903f5af88dd341a1caca
pmatyus <>
2021-04-15 10:58:52 +0000

    Completed exercise

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #write your code below this line
+    first = int(input('Give the first number:'))
+    second = int(input('Give the second number:'))
+    print(str(first) + ' + ' + str(second) + ' = ' + str(first + second))
+    print(str(first) + ' - ' + str(second) + ' = ' + str(first - second))
+    print(str(first) + ' * ' + str(second) + ' = ' + str(first * second))
+    print(str(first) + ' / ' + str(second) + ' = ' + str(first / second))
 
 if __name__ == '__main__':
     main()
